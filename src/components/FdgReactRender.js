eb7d0c1dad8ce1bd337c143ddb5c65699735ebf9
Jay Chung <xjkit54780@gmail.com>
2017-08-31 16:05:12 +0800

    Add function to append new data

--- src/components/FdgReactRender.js
+++ src/components/FdgReactRender.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import fake from '../fake/FdgReactRender';
 import * as d3 from 'd3';
 
 class FdgReactRender extends Component {
@@ -9,6 +8,9 @@ class FdgReactRender extends Component {
     height: PropTypes.number,
     linkDistance: PropTypes.number,
     forceStrength: PropTypes.number,
+    nodes: PropTypes.array,
+    links: PropTypes.array,
+    genData: PropTypes.func,
   };
   static defaultProps = {
     width: 600,
@@ -18,69 +20,94 @@ class FdgReactRender extends Component {
   };
   constructor(props) {
     super(props);
+    // nodes and links are deeply copied to the props from the parent component states
     this.state = {
-      nodes: fake.nodes,
-      links: fake.links,
+      nodes: props.nodes,
+      links: props.links,
     };
     this.force = null;
+    this.isUpdateData = false;
   }
   componentDidMount() {
-    this.force = d3.forceSimulation(this.state.nodes)
-      .force('charge', d3.forceManyBody()
-        .strength(this.props.forceStrength))
-      .force('link', d3.forceLink()
-        .distance(this.props.linkDistance)
-        .links(this.state.links))
-      .force('center', d3.forceCenter(this.props.width / 2, this.props.height / 2));
-    // ?????
-    this.force.on('tick', () => this.setState({
-      links: this.state.links,
-      nodes: this.state.nodes,
-    }));
+    this.renderGraph();
+  }
+
+  componentWillReceiveProps({ nodes, links }) {
+    this.setState({ nodes, links });
+    this.isUpdateData = true;
+  }
+
+  componentDidUpdate() {
+    if (this.isUpdateData) {
+      this.isUpdateData = false;
+      this.renderGraph();
+    }
+    console.log('componentDidUpdate');
   }
 
   componentWillUnmount() {
     this.force.stop();
   }
 
+  renderGraph = () => {
+    this.force = d3.forceSimulation(this.state.nodes)
+    .force('charge', d3.forceManyBody()
+      .strength(this.props.forceStrength))
+    .force('link', d3.forceLink()
+      .distance(this.props.linkDistance)
+      .links(this.state.links))
+    .force('center', d3.forceCenter(this.props.width / 2, this.props.height / 2));
+  // ?????
+    this.force.on('tick', () => this.setState({
+      links: this.state.links,
+      nodes: this.state.nodes,
+    }));
+  }
+
   render() {
     return (
-      <svg
-        width={this.props.width}
-        height={this.props.height}
-      >
-      {this.state.links.map((link,index) => (
-        <line
-          x1={link.source.x}
-          y1={link.source.y}
-          x2={link.target.x}
-          y2={link.target.y}
-          key={`line-${index}`}
-          stroke="black"
-        />
-      ))}
-      {this.state.nodes.map((node, index) => (
-        <circle
-          r={node.r}
-          cx={node.x}
-          cy={node.y}
-          fill="red"
-          key={`circle-${index}`}
-          onClick={() => console.log(`you click index ${index}`)}
-          style={{ cursor: 'pointer' }}
-        />
-      ))}
-      {this.state.nodes.map((node, index) => (
-        <text
-          x={node.x}
-          y={node.y}
-          dx={20}
-          textAnchor="middle"
-          fill="orange"
-          key={`text-${index}`}
-        >{index}</text>
-      ))}
-      </svg>
+      <div>
+        <svg
+          width={this.props.width}
+          height={this.props.height}
+        >
+        {this.state.links.map((link,index) => (
+          <line
+            x1={link.source.x}
+            y1={link.source.y}
+            x2={link.target.x}
+            y2={link.target.y}
+            key={`line-${index}`}
+            stroke="black"
+          />
+        ))}
+        {this.state.nodes.map((node, index) => (
+          <circle
+            r={node.r}
+            cx={node.x}
+            cy={node.y}
+            fill="red"
+            key={`circle-${index}`}
+            onClick={() => window.alert(`you click index ${index}`)}
+            style={{ cursor: 'pointer' }}
+          />
+        ))}
+        {this.state.nodes.map((node, index) => (
+          <text
+            x={node.x}
+            y={node.y}
+            dx={20}
+            textAnchor="middle"
+            fill="orange"
+            key={`text-${index}`}
+          >{index}</text>
+        ))}
+        </svg>
+        <div className="btn-group">
+          <button onClick={this.props.genData}>Generate</button>
+          <button onClick={this.props.appendData}>Append</button>
+        </div>
+      </div>
     );
   }
 }
--- src/fake/FdgReactRender.js
+++ src/fake/FdgReactRender.js
@@ -1,28 +1,31 @@
-const nodeCount = 20;
-const nodes = [];
 
-// generate 'nodeCount' nodes
+// generate initial nodes
 
-for (let i = 0; i < nodeCount; i++) {
-  nodes.push({
-    r: 10,
-    x: 0,
-    y: 0
-  });
+export const genNodesAndLinks = (count = 0) => {
+  const nodeCount = count || Math.ceil(Math.random() * 20);
+  const nodes = [];
+  const links = [];
+
+  // generate random nodes
+  for (let i = 0; i < nodeCount; i++) {
+    nodes.push({
+      r: 10,
+      x: 0,
+      y: 0
+    });
+  }
+  // generate initial links
+  for (let i = 0; i < nodeCount; i++) {
+    let target = 0;
+    do {
+      target = Math.floor(Math.random() * nodeCount)
+    } while(target === i)
+
+    links.push({
+      source: i,
+      target,
+    });
+  }
+
+  return { nodes, links };
 }
-
-const links = [];
-
-for (let i = 0; i < nodeCount; i++) {
-  let target = 0;
-  do {
-    target = Math.floor(Math.random() * nodeCount)
-  } while(target === i)
-
-  links.push({
-    source: i,
-    target,
-  });
-}
-
-module.exports = { nodes, links };
